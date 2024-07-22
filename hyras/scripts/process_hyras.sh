5223228f3d4a4514876278d573ccdde4b776fae7
AlexDo1 <alexander.dolich@kit.edu>
2024-07-22 19:16:17 +0200

    small fixesy

--- hyras/scripts/process_hyras.sh
+++ hyras/scripts/process_hyras.sh
@@ -11,8 +11,8 @@ echo "[$(date +%F\ %T)] Starting processing of HYRAS meteorological data for the
 # Start parallel extraction and processing of HYRAS data
 echo "[$(date +%T)] Extracting and processing HYRAS data..."
 python /scripts/01_papermill_execute_parallel.py
-cp /scripts/01_papermill_execute_parallel.R /output_data/scripts/01_papermill_execute_parallel.R
-echo "[$(date +%T)] Saved extracted and processed HYRAS data for all CAMELS-DE stations with 02_papermill_execute_parallel.R"
+cp /scripts/01_papermill_execute_parallel.py /output_data/scripts/01_papermill_execute_parallel.py
+echo "[$(date +%T)] Saved extracted and processed HYRAS data for all CAMELS-DE stations with 01_papermill_execute_parallel.py"
 
 # Create metadata file for hyras run and copy hyras data to CAMELS-DE station folders
 echo "[$(date +%T)] Creating metadata file for hyras run and copying hyras data to CAMELS-DE station folders..."
@@ -20,7 +20,7 @@ python /scripts/02_add_hyras_to_stations_and_create_metadata.py
 cp /scripts/02_add_hyras_to_stations_and_create_metadata.py /output_data/scripts/02_add_hyras_to_stations_and_create_metadata.py
 echo "[$(date +%T)] Saved metadata file for hyras run and copied hyras data to CAMELS-DE station folders with 02_add_hyras_to_stations_and_create_metadata.py"
 
-# Copy scripts to /camelsp/output_data/scripts/soils/isric/
+# # Copy scripts to /camelsp/output_data/scripts/meteo/hyras
 mkdir -p /camelsp/output_data/scripts/meteo/hyras/
 cp /output_data/scripts/* /camelsp/output_data/scripts/meteo/hyras/
 
